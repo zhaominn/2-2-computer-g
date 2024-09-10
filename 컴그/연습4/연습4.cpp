6c2ce29054e0cb5dac94c0670bdc0236c826f429
zhaominn <somin207@naver.com>
2024-09-10 18:49:32 +0900

    연습 4 끝냈다!! 우하하.

--- 컴그/연습4/연습4.cpp
+++ 컴그/연습4/연습4.cpp
@@ -2,23 +2,41 @@
 #include <string>
 #include <random>
 
+#include <sstream>
+#include <vector>
+
+#include <thread>
+#include <chrono>
+
+
 using namespace std;
 
 struct Node {
-	string location;
+	char location;
 	string shape;
-	int valid;
-	Node() : valid(-1) {}
+	char valid;
+	Node() : location(), shape(), valid(-1) {}
 };
 
-int main() {
-	Node board[5][5];
-
+void SettingBoard(Node board[5][5]) {
 	// 랜덤 설정
-	uniform_int_distribution <> W(1, 12);
-	static mt19937 gen(time(NULL));
+	random_device rd;
+	default_random_engine dre{ rd() };
+	uniform_int_distribution <> uid(1, 12);
+	uniform_int_distribution <> joker(1, 5);
 
-	// 보드 그리기
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 5; ++j) {
+			board[i][j].valid = -1;
+			board[i][j].shape = 'A' + uid(dre);
+		}
+	}
+
+	board[joker(dre)][joker(dre)].shape = '@';
+
+}
+
+void PrintBoard(Node board[5][5]) {
 	cout << "   a  b  c  d  e\n";
 	for (int i = 0; i < 5; ++i) {
 		cout << i + 1;
@@ -26,36 +44,49 @@ int main() {
 			if (board[i][j].valid == -1)
 				cout << "  *";
 			else
-				cout << " " << board[i][j].shape;
+				cout << "  " << board[i][j].shape;
 
-			board[i][j].location = { ("a" + i), j };
-			board[i][j].shape = W(gen);
-
-			cout<<board
 		}
 		cout << '\n';
 	}
+}
 
+int main() {
+	Node board[5][5];
+
+	SettingBoard(board);
+	PrintBoard(board);
 
 	string word1, word2;
 	while (1) {
-		cout << "좌표 입력 :";
-		cin >> word1 >> word2;
+		cout << "\n좌표 입력 :";
+		cin >> word1;
 
-		for (int i = 0; i < 5; ++i) {
-			cout << i + 1;
-			for (int j = 0; j < 5; ++j) {
-				if (board[i][j].valid == -1)
-					cout << "  *";
-				else
-					cout << " " << board[i][j].shape;
-
-				board[i][j].shape = W(gen);
-			}
-			cout << '\n';
+		if (word1 == "r") {
+			cout << "\n\n보드를 다시 세팅합니다~\n\n";
+			SettingBoard(board);
+			PrintBoard(board);
 		}
+		else {
+			cin >> word2;
 
+			if ((board[word1[1] - '1'][word1[0] - 'a'].shape == board[word2[1] - '1'][word2[0] - 'a'].shape)
+				|| board[word1[1] - '1'][word1[0] - 'a'].shape == "@" || board[word2[1] - '1'][word2[0] - 'a'].shape == "@") {
+				board[word1[1] - '1'][word1[0] - 'a'].valid = 1;
+				board[word2[1] - '1'][word2[0] - 'a'].valid = 1;
+			}
+			else {
+				board[word1[1] - '1'][word1[0] - 'a'].valid = 1;
+				board[word2[1] - '1'][word2[0] - 'a'].valid = 1;
+				PrintBoard(board);
+				cout << "\n다른 값이네요~ 다시 해보세요~~\n\n";
+				this_thread::sleep_for(3s);
+				board[word1[1] - '1'][word1[0] - 'a'].valid = -1;
+				board[word2[1] - '1'][word2[0] - 'a'].valid = -1;
+			}
+
+			PrintBoard(board);
+		}
 	}
-
 
 }
