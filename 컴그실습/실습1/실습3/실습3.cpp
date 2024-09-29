1d2df4963c5ecff2702b78a4b4990991dbb6c9d2
zhaominn <somin207@naver.com>
2024-09-29 10:39:47 +0900

    랜덤으로 정사각형 출력(윈도우 정사각형으로 설정)

--- 컴그실습/실습1/실습3/실습3.cpp
+++ 컴그실습/실습1/실습3/실습3.cpp
@@ -7,6 +7,31 @@ GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
 void TimerFunction(int value);
 
+#include <random>
+#include <cmath>
+
+struct rectangle {
+	GLfloat startX;
+	GLfloat startY;
+	GLfloat endX;
+	GLfloat endY;
+	GLfloat r;
+	GLfloat g;
+	GLfloat b;
+};
+
+rectangle Rectangles[10]{
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f},
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f},
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f},
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f},
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f},
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f},
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f},
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f},
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f},
+	{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f}
+};
 
 void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 {
@@ -14,7 +39,7 @@ void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 	glutInit(&argc, argv); //--- glut 초기화
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //--- 디스플레이 모드 설정
 	glutInitWindowPosition(0, 0); //--- 윈도우의 위치 지정
-	glutInitWindowSize(800, 600); //--- 윈도우의 크기 지정
+	glutInitWindowSize(800, 800); //--- 윈도우의 크기 지정
 	glutCreateWindow("Example1"); //--- 윈도우 생성(윈도우 이름)
 	//--- GLEW 초기화하기
 	glewExperimental = GL_TRUE;
@@ -39,8 +64,17 @@ void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 {
 	//--- 변경된 배경색 설정
-	glClearColor(1.0f,1.0f,1.0f, 1.0f); //--- 바탕색을 변경
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f); //--- 바탕색을 변경
 	glClear(GL_COLOR_BUFFER_BIT); //--- 설정된 색으로 전체를 칠하기
+
+	//-------------------
+
+	for (int i = 0; i < 10; ++i) {
+		glColor3f(Rectangles[i].r, Rectangles[i].g, Rectangles[i].b);
+		glRectf(Rectangles[i].startX,Rectangles[i].startY,Rectangles[i].endX,Rectangles[i].endY);
+	}
+
+	//-------------------
 	glutSwapBuffers(); //--- 화면에 출력하기
 }
 
@@ -52,8 +86,20 @@ GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
 GLvoid Keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
-	
+	case 'a':
+		std::random_device rd;
+		std::mt19937 gen(rd());
 
+		std::uniform_real_distribution<> dis(-9.0, 9.0);
+		for (int i = 0; i < 10; ++i) {
+			GLfloat x = round(dis(gen) * 10) / 100.0;
+			GLfloat y = round(dis(gen) * 10) / 100.0;
+			GLfloat r = (rand() % 10) / 10.0f;
+			GLfloat g = (rand() % 10) / 10.0f;
+			GLfloat b = (rand() % 10) / 10.0f;
+			Rectangles[i] = {x,y,x+0.1f,y+0.1f,r,g,b };
+		}
+		break;
 	}
 	glutPostRedisplay(); //--- 배경색이 바뀔 때마다 출력 콜백 함수를 호출하여 화면을 refresh 한다
 }
